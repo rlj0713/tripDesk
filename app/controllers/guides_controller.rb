2e08a0d4a4385da4e31a2cd03588384a235f0e15
Reid <rlj0713@gmail.com>
2021-10-07 18:05:20 -0400

    updated guide controller to include restful routes

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,6 +1,42 @@
 class GuidesController < ApplicationController
+    def show
+        @guide = Guide.find_by_id(params[:id])
+        render json: GuideSerializer.new(@guide)
+    end
+    
     def index
         guides = Guide.all
         render json: GuideSerializer.new(guides)
+    end
+
+    def new
+        @guide = Guide.new
+    end
+
+    def create
+        @guide = Guide.new(guide_params)
+        if @guide.valid?
+            @guide.save
+        end
+    end
+
+    def edit
+        @guide = Guide.find_by_id(params[:id])
+    end
+
+    def update
+        @guide = guide.find_by_id(params[:id])
+        @guide.update(guide_params)
+    end
+
+    def destroy
+        @guide = guide.find_by_id(params[:id])
+        @guide.destroy
+    end
+
+    private
+
+    def guide_params
+        params.require(:guide).permit(:first_name, :last_name)
     end
 end
--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,6 +1,42 @@
 class ReservationsController < ApplicationController
+    def show
+        @reservation = Reservation.find_by_id(params[:id])
+        render json: @reservation.to_json(except: [:created_at, :updated_at], include: [:guide, :customer])
+    end
+    
     def index
         reservations = Reservation.all
         render json: reservations.to_json(except: [:created_at, :updated_at], include: [:guide, :customer])
+    end
+
+    def new
+        @reservation = Reservation.new
+    end
+
+    def create
+        @reservation = Reservation.new(res_params)
+        if @reservation.valid?
+            @reservation.save
+        end
+    end
+
+    def edit
+        @reservation = Reservation.find_by_id(params[:id])
+    end
+
+    def update
+        @reservation = reservation.find_by_id(params[:id])
+        @reservation.update(res_params)
+    end
+
+    def destroy
+        @reservation = reservation.find_by_id(params[:id])
+        @reservation.destroy
+    end
+
+    private
+
+    def res_params
+        params.require(:reservation).permit(:reservation_date, :guide_id, :customer_id)
     end
 end
