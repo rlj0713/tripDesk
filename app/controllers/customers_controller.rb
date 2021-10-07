ba462bb5a9faa8c5cf254156236f198ec4087e14
Reid <rlj0713@gmail.com>
2021-10-07 18:18:03 -0400

    added show route to customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,17 @@
 class CustomersController < ApplicationController
+    def show
+        @customer = Customer.find_by_id(params[:id])
+        render json: CustomerSerializer.new(@customer)
+    end
+    
     def index
         customers = Customer.all
         render json: CustomerSerializer.new(customers)
+    end
+    
+    private
+
+    def customer_params
+        params.require(:customer).permit(:first_name, :last_name)
     end
 end
